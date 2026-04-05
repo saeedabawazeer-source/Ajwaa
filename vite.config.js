d6627291c1e342287d4239416d131bbcbe74f2da
Saeed Bawazer <saeed@Saeeds-MacBook-Pro.local>
2026-04-05 14:58:11 -0700

    fix: change vite base to relative so deployments work anywhere

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ajwaa/',
+  base: './', /* Uses relative paths so it works on Railway and Github Pages */
 })
